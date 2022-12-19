22be80412a10f761011c150c4e03141b0eb68d1e
Stanislav Mamulin <StanislavPM@ya.ru>
2022-12-19 20:40:51 +0500

    feat: add storing and receiving of the amount and quantity of all goods, changing the quantity of the selected item

--- src/components/controllers/cartController.ts
+++ src/components/controllers/cartController.ts
@@ -9,6 +9,8 @@ let instanceOfCart: CartController;
 
 class CartController {
     private cart: Map<number, CartProduct> = new Map();
+    private moneyAmount: number;
+    private totalProducts: number;
 
     constructor() {
         if (instanceOfCart) {
@@ -16,48 +18,81 @@ class CartController {
         }
 
         this.cart = new Map();
+        this.moneyAmount = 0;
+        this.totalProducts = 0;
     }
 
     addProductToCartByID(id: number, product: IProduct): void {
         if (this.cart.has(id)) {
-            this.cart.delete(id); // the product is already in the cart - delete it
+            // the product is already in the cart - delete it
+            this.quantityHasChangedByPcs(-1, this.cart.get(id) as CartProduct);
+            this.cart.delete(id);
         } else {
-            this.cart.set(id, {
+            const newProductInCart = {
                 ...product,
                 quantity: 1,
-            });
+            };
+
+            this.cart.set(id, newProductInCart);
+            this.quantityHasChangedByPcs(1, newProductInCart);
         }
     }
 
     addToCartByID(id: number): void {
         if (this.cart.has(id)) {
-            this.cart.delete(id); // the product is already in the cart - delete it
+            // the product is already in the cart - delete it
+            this.quantityHasChangedByPcs(-1, this.cart.get(id) as CartProduct);
+            this.cart.delete(id);
         } else {
             const product: IProduct | undefined = productsCollection.find((product) => product.id === id);
 
             if (product) {
-                this.cart.set(id, {
+                const newProductInCart = {
                     ...product,
                     quantity: 1,
-                });
+                };
+
+                this.cart.set(id, newProductInCart);
+                this.quantityHasChangedByPcs(1, newProductInCart);
             }
         }
+    }
+
+    private quantityHasChangedByPcs(pcs: number, product: CartProduct) {
+        this.moneyAmount += product.price * pcs;
+        this.totalProducts += pcs;
     }
 
     /**
      * Change the quantity of the product in the cart
      * @param id - Product ID
-     * @param quantity - How much to change the amount
+     * @param quantity - How much to change the amount (-1/+1)
      */
     changeQuantityById(id: number, quantity: number): void {
-        const product = this.cart.get(id);
+        const product: CartProduct | undefined = this.cart.get(id);
 
         if (product) {
-            this.cart.set(id, {
-                ...product,
-                quantity: product.quantity + quantity,
-            });
+            const newQuantity: number = product.quantity + quantity;
+            this.quantityHasChangedByPcs(quantity, product);
+
+            if (newQuantity <= 0) {
+                this.cart.delete(id); // delete the product
+            } else {
+                // change the quantity
+                this.cart.set(id, {
+                    ...product,
+                    quantity: newQuantity,
+                });
+            }
         }
+    }
+
+    public getTotalProductsInCart(): number {
+        return this.totalProducts;
+    }
+
+    public getMoneyAmount(): number {
+        return this.moneyAmount;
     }
 }
 
