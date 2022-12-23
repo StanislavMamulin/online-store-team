847498a2ddca99744282306f012d3b5054d11bb9
Stanislav Mamulin <StanislavPM@ya.ru>
2022-12-23 13:14:10 +0500

    refactor: move common functions to separate helpers

--- src/components/controllers/productsController.ts
+++ src/components/controllers/productsController.ts
@@ -1,3 +1,5 @@
+import { loweredArrayValues, toggleValueInArray } from '../../helpers/arrayHelpers';
+import { isNumberInRange, isStringInArray } from '../../helpers/checkers';
 import { productsCollection } from '../products';
 import { IProduct } from '../types';
 
@@ -26,16 +28,6 @@ export class ProductsController {
         this.allFilters.set('stock', this.filterByStock);
     }
 
-    private toggleFilter(filter: string[], newValue: string) {
-        const index = filter.findIndex((value) => value === newValue);
-
-        if (index === -1) {
-            filter.push(newValue);
-        } else {
-            filter.splice(index, 1);
-        }
-    }
-
     private changeLimitOfRange(range: [number, number], newLimit: [number, number]) {
         console.log('change limit of range');
     }
@@ -43,9 +35,9 @@ export class ProductsController {
     setFilterForField(field: keyof IProduct, filterValue: string | [number, number]) {
         if (typeof filterValue === 'string') {
             if (field === 'brand') {
-                this.toggleFilter(this.brandsForFilter, filterValue);
+                toggleValueInArray<string>(this.brandsForFilter, filterValue);
             } else if (field === 'category') {
-                this.toggleFilter(this.categoriesForFilter, filterValue);
+                toggleValueInArray<string>(this.categoriesForFilter, filterValue);
             }
         }
 
@@ -66,22 +58,6 @@ export class ProductsController {
 
         return isCategoryFilter || isBrandFilter;
     }
-
-    private isProductInArray = (fieldOfProduct: string, filters: string[]): boolean => {
-        if (filters.length === 0) {
-            return true;
-        }
-
-        return this.loweredArrayValues(filters).includes(fieldOfProduct.toLowerCase());
-    };
-
-    private isPropertyInRange = (propertyOfProduct: number, range: number[]): boolean => {
-        if (range.length === 0) {
-            return true;
-        }
-
-        return propertyOfProduct >= Math.min(...range) && propertyOfProduct <= Math.max(...range);
-    };
 
     private normalizeField(field: string): string {
         let searchField: string = field;
@@ -109,13 +85,13 @@ export class ProductsController {
     }
 
     private filterByCategory = (product: IProduct): boolean =>
-        this.isProductInArray(product.category, this.categoriesForFilter);
+        isStringInArray(product.category, this.categoriesForFilter);
 
-    private filterByBrand = (product: IProduct): boolean => this.isProductInArray(product.brand, this.brandsForFilter);
+    private filterByBrand = (product: IProduct): boolean => isStringInArray(product.brand, this.brandsForFilter);
 
-    private filterByPrice = (product: IProduct): boolean => this.isPropertyInRange(product.price, this.priceRange);
+    private filterByPrice = (product: IProduct): boolean => isNumberInRange(product.price, this.priceRange);
 
-    private filterByStock = (product: IProduct): boolean => this.isPropertyInRange(product.stock, this.stockRange);
+    private filterByStock = (product: IProduct): boolean => isNumberInRange(product.stock, this.stockRange);
 
     private filterProducts() {
         this.filteredProducts = Array(...this.products);
@@ -125,13 +101,9 @@ export class ProductsController {
         }
     }
 
-    private loweredArrayValues(array: string[]) {
-        return array.map((value) => value.toLowerCase());
-    }
-
     getAllValuesFromField(field: string) {
         const values = this.products.map((product) => product[field as keyof IProduct]);
-        return new Set(this.loweredArrayValues(values as Array<string>));
+        return new Set(loweredArrayValues(values as Array<string>));
     }
 
     getCountValuesFromProduct(field: string, value: string, filtered: boolean) {
@@ -162,6 +134,5 @@ export class ProductsController {
 
             return productValues.some((value) => value.includes(searchRequest.toLowerCase()));
         });
-        console.log(this.filteredProducts);
     }
 }
---
+++ src/helpers/arrayHelpers.ts
@@ -0,0 +1,13 @@
+export const loweredArrayValues = (array: string[]) => {
+    return array.map((value) => value.toLowerCase());
+};
+
+export const toggleValueInArray = <T>(array: T[], value: T) => {
+    const index = array.findIndex((arrayValue) => arrayValue === value);
+
+    if (index === -1) {
+        array.push(value);
+    } else {
+        array.splice(index, 1);
+    }
+};
